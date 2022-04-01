90203c344940223ef86146609b03b369087f2fc2
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-04-01 11:14:39 +0200

    Add primitive AFX expansion for predefined tags

--- Resources/Private/Plugin/src/CodeEditorWrap.js
+++ Resources/Private/Plugin/src/CodeEditorWrap.js
@@ -29,6 +29,51 @@ export default class CodeEditorWrap extends PureComponent {
             ['html', 'php'],
         )
 
+        const afxSnippets = {
+            'Neos.Fusion:Loop': `<Neos.Fusion:Loop items={}></Neos.Fusion:Loop>`,
+            'Neos.Fusion:Value': `<Neos.Fusion:Value value={}/>`,
+            'Neos.Fusion:Join': `<Neos.Fusion:Join></Neos.Fusion:Join>`,
+            'Neos.Fusion:Tag': `<Neos.Fusion:Tag tagName="" attributes.class=""></Neos.Fusion:Tag>`,
+        }
+
+        monaco.languages.registerCompletionItemProvider('html', {
+            provideCompletionItems: (model, position) => {
+                const word = model.getWordUntilPosition(position);
+
+                const prevChar = model.getValueInRange({
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: word.startColumn - 1,
+                    endColumn: word.startColumn
+                });
+
+                const prepareSnippet = snippet => {
+                    if (prevChar === '<' && snippet[0] === '<') {
+                        // if the char before the word was a `<`
+                        // we will not need it anymore from the snippet
+                        return snippet.slice(1)
+                    }
+                    return snippet;
+                }
+
+                const range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: word.startColumn,
+                    endColumn: word.endColumn
+                };
+
+                return {
+                    suggestions: Object.entries(afxSnippets).map(([label, snippet]) => ({
+                        label,
+                        range,
+                        insertText: prepareSnippet(snippet),
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                    }))
+                };
+            }
+        });
+
         const editor = monaco.editor.create(this.monacoContainer, {
             value: this.props.value,
             language: this.props.language,
