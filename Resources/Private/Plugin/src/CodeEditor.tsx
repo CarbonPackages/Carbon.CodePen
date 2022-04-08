58c1b9a8214611d9eefdb588891bed8a216ade93
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-04-08 17:02:52 +0200

    Almost compiling TS for CodeEditor.tsx

--- Resources/Private/Plugin/src/CodeEditor.tsx
+++ Resources/Private/Plugin/src/CodeEditor.tsx
@@ -1,52 +1,50 @@
-import React, {PureComponent} from 'react';
-import {neos} from '@neos-project/neos-ui-decorators';
-import PropTypes from 'prop-types';
-import {Button, Icon, Label} from '@neos-project/react-ui-components/';
-import {$transform} from "plow-js";
-import {connect} from 'react-redux';
-import {selectors} from '@neos-project/neos-ui-redux-store';
+import React from "react";
+import { neos, NeosInjectedProps } from "@neos-project/neos-ui-decorators";
+import { Button, Icon, Label } from "@neos-project/react-ui-components";
+import { connect, DefaultRootState } from "react-redux";
+import { selectors } from "@neos-project/neos-ui-redux-store";
+import { EditorProps } from "@neos-project/neos-ui-editors";
+import { GlobalRegistry, Node } from "@neos-project/neos-ts-interfaces";
+import { SynchronousRegistry } from "@mhsdesign/esbuild-neos-ui-extensibility/@neos-project/neos-ui-extensibility/src/registry";
 
-@neos(globalRegistry => ({
-    secondaryEditorsRegistry: globalRegistry.get('inspector').get('secondaryEditors')
-}))
-@connect($transform({
-    node: selectors.CR.Nodes.focusedSelector
-}))
-export default class CodeEditor extends PureComponent {
+type NeosProps = NeosInjectedProps<typeof mapRegistryToProps>;
 
-    static propTypes = {
-        node: PropTypes.object.isRequired,
-        identifier: PropTypes.string.isRequired,
-        renderSecondaryInspector: PropTypes.func.isRequired,
-        commit: PropTypes.func.isRequired,
-        onEnterKey: PropTypes.func.isRequired,
-        value: PropTypes.string,
-        secondaryEditorsRegistry: PropTypes.object.isRequired,
-        options: PropTypes.object,
-    };
+type StateProps = {
+    node?: Node;
+};
 
-    handleToggleCodeEditor = () => {
-        const {secondaryEditorsRegistry, renderSecondaryInspector} = this.props;
-        const {component: CodeEditorWrap} = secondaryEditorsRegistry.get('Carbon.CodeEditor/CodeEditorWrap');
+type OwnProps = EditorProps<{
+    language: string;
+}>;
 
-        renderSecondaryInspector('CARBON_CODEEDITOR_EDIT', () =>
+type Props = OwnProps & NeosProps & StateProps;
+
+class CodeEditor extends React.Component<Props> {
+    public handleToggleCodeEditor = (): void => {
+        const { secondaryEditorsRegistry, renderSecondaryInspector } =
+            this.props;
+        const { component: CodeEditorWrap } = secondaryEditorsRegistry.get(
+            "Carbon.CodeEditor/CodeEditorWrap"
+        );
+
+        renderSecondaryInspector("CARBON_CODEEDITOR_EDIT", () => (
             <CodeEditorWrap
-                id={this.props.node.contextPath + this.props.identifier}
+                id={this.props.node!.contextPath + this.props.identifier}
                 value={this.props.value}
-                onChange={value => this.props.commit(value)}
-                onSave={ this.props.onEnterKey}
+                onChange={(value: string) => this.props.commit(value)}
+                onSave={this.props.onEnterKey}
                 onToggleEditor={this.handleToggleCodeEditor}
                 language={this.props.options.language}
             />
-        );
-    }
+        ));
+    };
 
     render() {
         return (
             <div>
                 <Label>
-                    <Button  onClick={this.handleToggleCodeEditor}>
-                        <Icon icon="pencil" padded="right" title="Edit"/>
+                    <Button onClick={this.handleToggleCodeEditor}>
+                        <Icon icon="pencil" padded="right" title="Edit" />
                         edit code
                     </Button>
                 </Label>
@@ -54,3 +52,20 @@ export default class CodeEditor extends PureComponent {
         );
     }
 }
+
+const mapRegistryToProps = (globalRegistry: GlobalRegistry) => ({
+    secondaryEditorsRegistry: globalRegistry
+        .get("inspector")
+        .get("secondaryEditors") as SynchronousRegistry<"">,
+});
+
+const mapStateToProps = (state: DefaultRootState): StateProps => ({
+    node: selectors.CR.Nodes.focusedSelector(state),
+});
+
+const WrappedNeos = neos<OwnProps, NeosProps>(mapRegistryToProps)(CodeEditor);
+const WrappedState = connect<StateProps, {}, OwnProps>(mapStateToProps)(
+    WrappedNeos
+);
+
+export default WrappedState;
