cfb4542fde241e0edc3f675d5087bc0e75f61473
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-04-19 16:06:57 +0200

    DOCS: Add documentation

---
+++ Documentation/buildJsonTailwindConfig.js
@@ -0,0 +1,20 @@
+/**
+ * Json build script for "tailwind.config.js"
+ *
+ * `node buildJsonTailwindConfig.js ./Packages/Sites/Neos.Demo/Resources/Public/clientTailwindConfig.json`
+ */
+
+const fs = require("fs");
+const config = require("./tailwind.config.js");
+
+const [, , outfile] = process.argv;
+
+const { theme } = config;
+
+// we select only the theme option for json, as plugins cannot be converted to json.
+// if you need plugins, look at building a worker with embedded config
+const clientTailwind = {
+    theme,
+};
+
+fs.writeFileSync(outfile, JSON.stringify(clientTailwind));
