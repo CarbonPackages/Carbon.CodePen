11907ca5b6cff8e3076611800524c61f0d0b5404
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-05-07 10:42:22 +0200

    Dont persist changes of node->setProperty when previewing

--- Classes/Controller/PreviewController.php
+++ Classes/Controller/PreviewController.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Carbon\CodePen\Controller;
 
-use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Model\Node;
 use Neos\Flow\Mvc\Controller\ActionController;
 use Neos\Neos\View\FusionView;
 use Neos\Flow\Annotations as Flow;
@@ -24,12 +24,31 @@ class PreviewController extends ActionController
      */
     protected $fusionRootPath;
 
-    public function renderAction(NodeInterface $node, string $propertyName, string $propertyValue)
+    public function renderAction(Node $node, string $propertyName, string $propertyValue)
     {
-        $propertyValueDecoded = json_decode($propertyValue, true);
-        // todo unwanted side effect!
-        $node->setProperty($propertyName, $propertyValueDecoded);
+        $propertyValueDecoded = json_decode($propertyValue, true, 512, JSON_THROW_ON_ERROR);
+        if ($propertyValueDecoded === '' || $propertyValueDecoded === []) {
+            $propertyValueDecoded = null;
+        }
+        $mockedNode = $this->getMockedNodeWithProperty($node, $propertyName, $propertyValueDecoded);
         $this->view->setFusionPath($this->fusionRootPath);
-        $this->view->assign('value', $node);
+        $this->view->assign('value', $mockedNode);
+    }
+
+    /**
+     * See why this implementation was chosen
+     * @link {https://neos-project.slack.com/archives/C050C8FEK/p1650722458764259}
+     */
+    private function getMockedNodeWithProperty(Node $node, string $propertyName, $propertyValue)
+    {
+        $nodeData = $node->getNodeData();
+
+        $nodeDataPropertiesReflection = (new \ReflectionClass($nodeData))->getProperty('properties');
+        $nodeDataPropertiesReflection->setAccessible(true);
+        $properties = $nodeDataPropertiesReflection->getValue($nodeData);
+
+        $properties[$propertyName] = $propertyValue;
+        $nodeDataPropertiesReflection->setValue($nodeData, $properties);
+        return $node;
     }
 }
