c89024acbe0ac18f0708c3b49dfb43860c6c24e2
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-06-25 21:00:20 +0200

    WIP: Add Playwright with 3 working tests and session reuse

--- Tests/Integration/TestDistribution/configureSite.sh
+++
@@ -1,4 +0,0 @@
-./flow database:setcharset
-./flow doctrine:migrate
-./flow site:create --name testSite --package-key Carbon.TestSite --node-type Carbon.TestSite:Page
-./flow user:create --roles Neos.Neos:Administrator admin admin Jon Doe
