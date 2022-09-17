8062966b78bbdc67374b3de9003c7f1eb5026397
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-09-17 17:28:04 +0200

    Tests: Introduce dynamic FLOW_CONTEXT switch via HttpHeader to test json tailwind css config

---
+++ Tests/Integration/TestDistribution/tailwind.config.forJson.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'marcherryJson': '#D2042D',
+            },
+            spacing: {
+              'makeTheButtonBiggerJson': '42rem',
+            },
+        },
+    },
+}
