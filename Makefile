4c5785178dbdacc27aff7ce87e69ffdd83c963f8
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-09-18 11:58:17 +0200

    Improve test runner

--- Makefile
+++ Makefile
@@ -1,26 +1,61 @@
+#				#
+# Plugin build	#
+#				#
 
 PLUGIN = Resources/Private/Plugin
 
-build:
+install:
 	cd ${PLUGIN} && . ${NVM_DIR}/nvm.sh && nvm use
 	cd ${PLUGIN} && pnpm install
+
+build:
 	cd ${PLUGIN} && pnpm run build
 
 watch:
 	cd ${PLUGIN} && pnpm run watch
 
+
+#								#
+# Ddev local test distribution	#
+#								#
+
 DISTRIBUTION = Tests/Integration/TestDistribution
 
-distributionSetup:
+installDistribution:
 	cd ${DISTRIBUTION} && ddev composer install
+	cd ${DISTRIBUTION} && ddev exec ./flow doctrine:migrate
 	cd ${DISTRIBUTION} && ddev exec ./flow user:create --roles Neos.Neos:Administrator admin admin Jon Doe
-	cd ${DISTRIBUTION} && ddev exec ./softSiteDeleteAndSetup.sh
-
-distributionUp:
+	cd ${DISTRIBUTION} && ddev exec ./flow site:import --package-key Carbon.TestSite
+	
+up:
 	cd ${DISTRIBUTION} && ddev start
+	cd ${DISTRIBUTION} && ddev exec ./flow || echo "Hmmm"
 
-distributionDown:
+down:
 	cd ${DISTRIBUTION} && ddev stop
 
+cleanSite:
+	cd ${DISTRIBUTION} && ddev exec ./flow site:prune testsite
+	cd ${DISTRIBUTION} && ddev exec ./flow site:import --package-key Carbon.TestSite
+
+
+#				 	#
+# Playwright tests 	#
+#					#
+
+# @param {g} grep select a test
+# @example
+# 	test g="typing"
 test:
-	cd Tests/Integration && pnpm run test
+	make testChromiumHeaded
+
+# @param {g} grep select a test
+testChromiumHeaded:
+	cd Tests/Integration/ && pnpm playwright test --headed --project chromium -g "${g}"
+
+smokeTest:
+	cd Tests/Integration/ && pnpm playwright test --project chromium -g 'typing'
+
+installTests:
+	cd Tests/Integration/ && pnpm i
+	cd Tests/Integration/ && pnpm playwright install chromium firefox
--- Tests/Integration/playwright.config.ts
+++ Tests/Integration/playwright.config.ts
@@ -4,22 +4,17 @@ import path from 'path';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   // workers: 1,
   globalSetup: "./Features/setup.ts",
-  testDir: path.join(__dirname, 'Features'), /* Test directory */
-  forbidOnly: !!process.env.CI,                 /* Whether to exit with an error if any tests or groups are marked as test.only() or test.describe.only(). Useful on CI. */
-  retries: process.env.CI ? 2 : 0,              /* If a test fails on CI, retry it additional 2 times */
-  timeout: 30000,                        /* Timeout per test */
-  // outputDir: 'test-results/',                /* Artifacts folder where screenshots, videos, and traces are stored. */
+  testDir: path.join(__dirname, 'Features'),
+  retries: 0,
+  timeout: 10000,
 
-  /* Run your local dev server before starting the tests: */
-  /* https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests */
   webServer: {
-    command: 'pnpm run startNeos',
-    url: 'https://carboncodepentestdistribution.ddev.site',
+    command: 'cd ../.. && make up && make cleanSite',
+    url: process.env.BASE_URL,
     timeout: 120 * 1000,
     reuseExistingServer: true,
     ignoreHTTPSErrors: true,
@@ -44,38 +39,6 @@ const config: PlaywrightTestConfig = {
     //   name: 'firefox',
     //   use: {
     //     ...devices['Desktop Firefox'],
-    //   },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-    /* Test against stable browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
     //   },
     // },
   ],
