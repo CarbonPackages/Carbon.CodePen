2777a269d4fa4b4b67e4d53a786387d2a00dcf01
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-09-23 20:04:16 +0200

    Test: Cleanup fixtures - use  for CodePen::insert also paste

--- Tests/Integration/Features/fixture/ContentElement.ts
+++ Tests/Integration/Features/fixture/ContentElement.ts
@@ -3,7 +3,7 @@ import { CodePen } from "./CodePen";
 import { sleep } from "./sleep";
 
 export class ContentElement {
-    constructor(private page: Page, private contentMutationAllowed: boolean, private codePenState: { isInitialized: boolean }) {
+    constructor(private page: Page, private contentMutationAllowed: boolean) {
     }
 
     async apply() {
@@ -22,42 +22,17 @@ export class ContentElement {
     async withCodePen(callback: ((args: {codePen: CodePen}) => Promise<void>)) {
         await this.page.click('text=Open CodePen');
 
-        // await waitForDomEvent(this.codePenPreview, "load", {timeout: 10000})
-
         await this.codePenPreview.waitFor()
 
-        // if (!this.codePenState.isInitialized) {
-        //     this.codePenState.isInitialized = true;
-        //     await sleep(1500)
-        // }
-
-        const codePen = new CodePen(this.page);
-        
-        // while (true) {
-        //     await codePen.forceComplection()
-        //     try {
-        //         await this.codePenEditor.locator(`[widgetid="editor.widget.suggestWidget"]`).isVisible({ timeout: 100 })
-        //         break;
-        //     } catch (error) {
-        //     }
-        // }
-        // await this.codePenInput.press("Escape")
-
-
-        // todo find a relieable way to detemine
         await sleep(1500)
 
-        await callback({codePen})
+        await callback({codePen: new CodePen(this.page)})
 
         // auto close
         if (await this.codePenEditor.count() !== 0) {
             await this.page.click('text=Open CodePen')
             await sleep(100)
         }
-    }
-
-    private get codePenPreviewFrame() {
-        return this.secondaryInspector.frameLocator('iframe[src^="/neos/codePen"]');
     }
 
     private get codePenPreview() {
@@ -70,9 +45,5 @@ export class ContentElement {
 
     private get codePenEditor() {
         return this.secondaryInspector.locator('.monaco-editor[role="code"]')
-    }
-
-    private get codePenInput() {
-        return this.codePenEditor.locator('.inputarea')
     }
 }
