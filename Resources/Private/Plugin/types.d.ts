f03816b872f3503b11670fd77a01e166e3296dbd
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-01-23 15:25:12 +0100

    Task: Update Neos UI types.d.ts

--- Resources/Private/Plugin/types.d.ts
+++ Resources/Private/Plugin/types.d.ts
@@ -105,6 +105,11 @@ declare module "@neos-project/neos-ui-redux-store" {
         System: {
             authenticationTimeout(): void;
         };
+        UI: {
+            FlashMessages: {
+                add(key: string, message: string, severity: "success" |"info" | "error", timeout?: number)
+            }
+        }
     };
 
     export const actions: Actions;
@@ -126,7 +131,7 @@ declare module "@neos-project/neos-ui-decorators" {
 
     /**
      * Infers the type of props that a neosifier will inject into a component.
-     * we reuse this behavior from {@see ConnectedProps}
+     * we reuse this behavior from {@link ConnectedProps}
      */
     export type NeosifiedProps<TNeosifier> = ConnectedProps<TNeosifier>;
 
@@ -452,30 +457,114 @@ declare module "@neos-project/neos-ts-interfaces" {
         ) => string;
     }
 
+    /**
+     * Component Wiring
+     * 
+     * {@link https://docs.neos.io/cms/manual/extending-the-user-interface/react-extensibility-api#component-wiring}
+     */
     export type EditorProps<Options = {}, Value = any> = {
+        /**
+         * an identifier which can be used for HTML ID generation
+         * @example
+         *  "__neos__editor__property---{propertyName}"
+         */
         id?: string;
-        renderHelpIcon(): JSX.Element | "";
+        /** name of the node property to edit */
         identifier: string;
+        /**
+         * label of the node property
+         * to be possibly translated via i18n.translate(label)
+         * 
+         * recommended, when {@link RegisteredEditor.hasOwnLabel} = true is used
+         *
+         *  @example
+         * ```jsx
+         *  <Label htmlFor={this.props.id}>
+         *      <I18n id={this.props.label} />
+         *      {this.props.renderHelpIcon()}
+         *  </Label>
+         * ```
+         */
         label: string;
+        /** additional editor options of the `editorOptions` */
         options: Options;
+        /**
+         * currently edited property value of the node
+         * is reactive - eg updates on discard and commit
+         */
         value?: Value;
-        hooks?: object;
+        /**
+         * @param secondaryInspectorName identifier, used to implement toggling of the inspector when calling this method twice.
+         * @param secondaryInspectorComponentFactory factory for the secondary inspector content or undefined|null to close the secondary inspector
+         * @example
+         *  props.renderSecondaryInspector('IMAGE_CROPPING', () => <MySecondaryInspectorContent />)
+         */
         renderSecondaryInspector(
             secondaryInspectorName: string,
-            secondaryInspectorComponentFactory: () => JSX.Element
+            secondaryInspectorComponentFactory: () => JSX.Element | undefined | null
         ): void;
+        /**
+         * register value change of the node property, which can be applied or discarded
+         * 
+         * @param value the new value. 
+         * @param hooks an object whose keys are saveHooks to be triggered, the values are hook-specific options: Example: `{'Neos.UI:Hook.BeforeSave.CreateImageVariant': nextImage}`
+         */
+        commit(value: Value, hooks?: Record<string, unknown>): void;
+        /**
+         * unsets the property
+         * 
+         * @param value an empty string to unset the property
+         */
+        commit(value: "", hooks?: Record<string, unknown>): void;
+
+        // unofficial api
+        /** name of the editor `Foo.Bar/EditorName` */
         editor: string;
-        editorRegistry: object;
-        i18nRegistry: object;
+        /**
+         * renders toggleable button
+         * 
+         * see for an examle:
+         * {@link EditorProps.label}
+         */
+        renderHelpIcon(): JSX.Element | "";
+        hooks?: Record<string, unknown>;
+        editorRegistry: EditorRegistry;
+        i18nRegistry: I18nRegistry;
+        /** applies any pending changes (as if you clicked the apply button) */
         onEnterKey(): void;
         helpMessage?: string;
         helpThumbnail?: string;
+        /** the value has pending changes (was changed via commit) */
         highlight?: boolean;
-        commit(value: any, hooks?): void;
+        /**
+         * styles an orange or red box-shadow
+         * around the element the class is applied
+         * to indicate pending changes or failure on validation errors
+         * 
+         * @example
+         * ```css
+         *  box-shadow: 0 0 0 2px /^(red|orange)$/;
+         *  border-radius: 2px;
+         * ```
+         */
+        className: string;
     };
 
     interface RegisteredEditor {
+        /** Editor component to use */
         component: React.ComponentType<any>;
+        /**
+         * No label of the property and helpmessage will be rendered above the component.
+         * 
+         * You need to render the label internally in the component
+         * @example
+         * ```jsx
+         *  <Label htmlFor={this.props.id}>
+         *      <I18n id={this.props.label} />
+         *      {this.props.renderHelpIcon()}
+         *  </Label>
+         * ```
+         */
         hasOwnLabel?: boolean;
     }
 
