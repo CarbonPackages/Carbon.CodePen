3e04b14fb3a3cf06609e2b437fe3b85474913160
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-02-03 10:01:27 +0100

    Bugfix: out of band rendering fix styles determined by server not beeing generated

--- Resources/Private/Fusion/defaultCodePenPreviewHandler.js
+++ Resources/Private/Fusion/defaultCodePenPreviewHandler.js
@@ -3,32 +3,33 @@
 
 window.configureCodePenPreview((codePenContext) => {
     // Default behaviour:
-    // - inject tailwindstyles generated from all content tabs,
     // - render component server side
+    // - inject tailwindstyles generated from the component html
     codePenContext.onContentDidChange(async ({ tabValues, tabId }) => {
         // render component server side
         codePenContext.renderComponentOutOfBand().then((content) => {
             document.body.innerHTML = content;
-        });
-
-        // generate tailwind styles
-        codePenContext.library
-            .generateTailwindStylesFromContent?.(
-                `@tailwind base;
-                @tailwind components;
-                @tailwind utilities;`,
-                Object.values(tabValues)
-            )
-            .then((css) => {
-                const style = document.getElementById("_codePenTwStyle");
-                const newStyle = document.createElement("style");
-                newStyle.id = "_codePenTwStyle";
-                newStyle.innerHTML = css;
-                if (!style) {
-                    document.head.append(newStyle);
-                    return;
-                }
-                style.parentNode.replaceChild(newStyle, style);
-            });
+            return content;
+        }).then((content) => {
+            // generate tailwind styles
+            codePenContext.library
+                .generateTailwindStylesFromContent?.(
+                    `@tailwind base;
+                    @tailwind components;
+                    @tailwind utilities;`,
+                    [content]
+                )
+                .then((css) => {
+                    const style = document.getElementById("_codePenTwStyle");
+                    const newStyle = document.createElement("style");
+                    newStyle.id = "_codePenTwStyle";
+                    newStyle.innerHTML = css;
+                    if (!style) {
+                        document.head.append(newStyle);
+                        return;
+                    }
+                    style.parentNode.replaceChild(newStyle, style);
+                });
+        })
     }, 500);
 });
